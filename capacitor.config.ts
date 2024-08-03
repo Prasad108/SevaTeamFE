037c0158f0fef18664a3daffbab836e30b60de9c
pradukal <prasad.dukale@loblaw.ca>
2024-08-03 11:18:15 +0530

    Initial commit of Ionic project Seva Team FE

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.sevateamfe',
+  appName: 'sevateamfe',
+  webDir: 'www'
+};
+
+export default config;
